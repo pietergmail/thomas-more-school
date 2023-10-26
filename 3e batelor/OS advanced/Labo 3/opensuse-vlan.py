22d31fbf4e09506928e794286fbbe1a88c318ca8
PieterHerremans <33780619+pietergmail@users.noreply.github.com>
2023-10-26 12:12:07 +0200

    small fix

--- 3e batelor/OS advanced/Labo 3/opensuse-vlan.py
+++ 3e batelor/OS advanced/Labo 3/opensuse-vlan.py
@@ -27,10 +27,10 @@ with open("opensuse-vlan.txt", "w") as config_file:
             vlan_ip = str(subnet.network_address + 2)
 
             config_file.write(f"echo IPADDR= \'{vlan_ip}\\28\' >> {outputfile} \n")
-            config_file.write(f"echo BOOTPROTO=\'static\' > {outputfile} \n")
-            config_file.write(f"echo STARTMODE=\'hotplug\' > {outputfile} \n")
-            config_file.write(f"echo ZONE=public > {outputfile} \n")
-            config_file.write(f"echo VLAN=\'yes\' > {outputfile} \n")
-            config_file.write(f"echo ETHERDEVICE==\'eth1\' > {outputfile} \n")
+            config_file.write(f"echo BOOTPROTO=\'static\' >> {outputfile} \n")
+            config_file.write(f"echo STARTMODE=\'hotplug\' >> {outputfile} \n")
+            config_file.write(f"echo ZONE=public >> {outputfile} \n")
+            config_file.write(f"echo VLAN=\'yes\' >> {outputfile} \n")
+            config_file.write(f"echo ETHERDEVICE==\'eth1\' >> {outputfile} \n")
 
 print("Configuration file generated: opensuse-vlan.txt")
